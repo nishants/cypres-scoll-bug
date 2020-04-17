eed047238e924201082e1f8de8f4416d9de044c8
nishant <nishant.singh87@gmail.com>
2020-04-17 13:05:00 +0530

    Update cypress to 4.3.0.

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('test scroll bug', () => {
   it('should scroll to element and take screenshot', () => {
-    cy.visit('http://127.0.0.1:8080');
+    cy.visit('http://127.0.0.1:8085');
     cy.get('#footer').scrollIntoView();
     cy.screenshot({capture: 'viewport'})
   })
